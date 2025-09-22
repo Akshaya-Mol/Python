34a63c83576f765a559fb41382f7c315da31741d
Akshaya-Mol <molakshaya55@gmail.com>
2025-09-22 19:09:27 +0530

    Intial Commit

---
+++ task2.py
@@ -0,0 +1,13 @@
+attendance=[18,20,19,15,21]
+for x in attendance:
+    if x >= 20:
+        print("Full Attendace")
+    else:
+        print("Not Full")
+
+# a=0
+# for y in attendance:
+#     if(y>=20):
+#         a+=1
+# print("Full Day Count")
+    
